012c67b1c9713ac723e1201eef89cab680a7b1a2
alexandre menezes <alexandre.fmenezes@gmail.com>
2016-09-15 20:50:42 -0300

    Add files

---
+++ app/footerui.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "<nav class=\"navbar navbar-default navbar-static-bottom\">";
+  echo "<div class=\"container\">";
+    echo "<p class=\"navbar-text\">The OWASP Secure Headers Project</p>
+    <p class=\"navbar-text navbar-nav navbar-right\">
+      <a href=\"mailto:alexandre.fmenezes@owasp.org.br;ricardo.iramar@owasp.org.br\">
+        Team Contact
+      </a> |
+      <a href=\"https://www.github.com/secureheaders\">
+        Repository
+      </a>
+    </p>";
+  echo "</div>";
+echo "</nav>";
+
+ ?>
