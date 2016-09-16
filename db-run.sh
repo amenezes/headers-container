63293756e505d076ddcbb4fa737f00be02136068
alexandre menezes <alexandre.fmenezes@gmail.com>
2016-09-16 14:08:41 -0300

    Update db-run.sh

--- db-run.sh
+++ db-run.sh
@@ -2,5 +2,7 @@
 DCP_PRESENCE=$(find /usr/local/bin -name docker-compose | wc -l)
 if [ $DCP_PRESENCE -eq 1 ]
   then
-    /usr/local/bin/docker-compose exec mysql mysql/scripts/conf-db.sh
+    /usr/local/bin/docker-compose exec mysql bash mysql/scripts/conf-db.sh
+  else
+    echo "[*] some problem happened with script: <db-run.sh> :/"
 fi
