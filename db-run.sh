218ee46e82846870b8334b841ceb194f25938872
alexandre menezes <alexandre.fmenezes@gmail.com>
2016-09-16 12:25:46 -0300

    Update db-run.sh

--- db-run.sh
+++ db-run.sh
@@ -2,5 +2,5 @@
 DCP_PRESENCE=$(find /usr/local/bin -name docker-compose | wc -l)
 if [ $DCP_PRESENCE -eq 1 ]
   then
-    docker-compose exec mysql /opt/conf-db.sh
+    /usr/local/bin/docker-compose exec mysql /opt/conf-db.sh
 fi
