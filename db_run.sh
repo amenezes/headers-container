0fd30b53090ff592ab70463e1cdd54306321c676
Unknown <alexandre.fmenezes@gmail.com>
2016-09-16 22:00:15 -0300

    Script <db_run.sh> renamed

--- db_run.sh
+++ db_run.sh
@@ -4,5 +4,5 @@ if [ $DCP_PRESENCE -eq 1 ]
   then
     /usr/local/bin/docker-compose exec mysql bash /opt/conf-db.sh
   else
-    echo "[*] some problem happened with script: <db-run.sh> :/"
+    echo "[*] some problem happened with script: <db_run.sh> :/"
 fi
