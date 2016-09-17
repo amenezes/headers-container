367ae0442346514b9f682ae44ee05ce03ec08708
Unknown <alexandre.fmenezes@gmail.com>
2016-09-16 22:25:30 -0300

    Code refactored

--- get_db_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-CURL_PRESENCE=$(find /usr/bin -name curl | wc -l)
-if [ $CURL_PRESENCE -eq 1 ]
-  then
-    /usr/bin/curl -o headers.sql https://dl.dropboxusercontent.com/u/6427240/headers.sql
-    mv headers.sql mysql/scripts/headers.sql
-  else
-    echo "[*] some problem happened with script: <get_db_data.sh> :/"
-fi
