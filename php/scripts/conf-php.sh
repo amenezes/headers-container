ef329878255ab391ff78ef798f7798a409e44de5
Unknown <alexandre.fmenezes@gmail.com>
2016-09-18 21:40:04 -0300

    php-fpm image changed to alpine

--- php/scripts/conf-php.sh
+++ php/scripts/conf-php.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-echo "extension=mysql.so" > /usr/local/etc/php/conf.d/mysql.ini
 echo "extension=mysqli.so" > /usr/local/etc/php/conf.d/mysqli.ini
-echo "extension=mysqlnd.so" > /usr/local/etc/php/conf.d/mysqlnd.ini
