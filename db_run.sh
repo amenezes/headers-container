732998b4f9671f02455109d5629b6753e33e78b9
Unknown <alexandre.fmenezes@gmail.com>
2016-09-19 19:49:20 -0300

    Update build scripts

--- db_run.sh
+++ db_run.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-HEADERS_FILE_SIZE=$(wc -c 'mysql/scripts/headers.sql' | cut -d' ' -f1)
-if [ $HEADERS_FILE_SIZE -le 1 ]
-  then
-    curl -o headers.sql https://dl.dropboxusercontent.com/u/6427240/headers.sql
-    mv headers.sql mysql/scripts/headers.sql
-  else
-    echo "[*] latest version of file <headers.sql> already present."
-fi
-
 DCP_PRESENCE=$(find /usr/local/bin -name docker-compose | wc -l)
 if [ $DCP_PRESENCE -eq 1 ]
   then
