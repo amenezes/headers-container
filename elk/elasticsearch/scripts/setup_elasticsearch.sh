7329cc7f61b0d252ef0a14158c85bfc44e3b62e0
Unknown <alexandre.fmenezes@gmail.com>
2016-11-16 21:13:39 -0200

    Elasticsearch container added

---
+++ elk/elasticsearch/scripts/setup_elasticsearch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# --------------- create oshp repository ----------------
+curl -XPUT 'http://localhost:9200/_snapshot/oshp' -d '{
+    "type": "fs",
+    "settings": {
+        "location": "/usr/share/elasticsearch/repo/oshp",
+        "compress": true
+    }
+}'
+# --------------- restore snapshot data ----------------
+curl -XPOST 'http://localhost:9200/_snapshot/oshp/snapshot_2016_4_qtrly/_restore' -d'{
+    "indices": "oshp",
+    "ignore_unavailable": true,
+    "include_global_state": true
+}'
